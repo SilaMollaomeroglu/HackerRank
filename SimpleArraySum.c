399c32676ff98e11378282d479c7e0793fdbfc16
SilaMollaomeroglu <69357065+SilaMollaomeroglu@users.noreply.github.com>
2021-11-26 14:54:07 +0300

    Add files via upload

---
+++ SimpleArraySum.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	int n;
+	scanf("%d ",&n);
+   	if(n<0)
+    {
+    	printf("n>0 !");
+	}
+	int elements,i;
+	int sum = 0;
+	for(i=0;i<n;i++)
+	{
+	
+		scanf("%d",&elements);
+		if(elements>1000)
+		{
+			printf("elements<1000 !");
+			break;
+		}
+		sum = sum + elements;
+
+    }
+   	printf("%d",sum);
+
+
+
+	return 0;
+}
